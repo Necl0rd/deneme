436c03554a084ea8d9e3fdabbb2a0d026c238400
Necl0rd <brs.ycl@hotmail.com>
2024-05-27 21:29:55 +0300

    branch

--- src/Gun1/OyunApp.java
+++ src/Gun1/OyunApp.java
@@ -5,5 +5,6 @@ public class OyunApp
     public static void main(String[] args)
     {
         System.out.println("Bu bir oyun app'idirghjghjghj..");
+        System.out.println("branches");
     }
 }
