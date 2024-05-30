6faba12c69cbd1cffc477de15eda6080ab901f06
Necl0rd <brs.ycl@hotmail.com>
2024-05-31 00:30:51 +0300

    project updated

--- src/gun1/OyunApp.java
+++ src/gun1/OyunApp.java
@@ -4,6 +4,6 @@ public class OyunApp
 {
     public static void main(String[] args)
     {
-        System.out.println("Bu bir oyun app'idirghjghjghj..");
+        System.out.println("Bu bir oyun app");
     }
 }
--- src/gun1/YeniGun2.java
+++
@@ -1,8 +0,0 @@
-package gun1;
-
-public class YeniGun2
-{
-    public static void main(String[] args) {
-        System.out.println("as selam");
-    }
-}
