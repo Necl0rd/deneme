8b85c5da77359cffcabd87b322f462effd4fa35e
Necl0rd <brs.ycl@hotmail.com>
2024-05-30 15:37:20 +0300

    update

--- src/gun1/YeniGun2.java
+++ src/gun1/YeniGun2.java
@@ -4,5 +4,7 @@ public class YeniGun2
 {
     public static void main(String[] args) {
         System.out.println("selamlar");
+        System.out.println("selam as selam as");
+        System.out.println("as selam");
     }
 }
